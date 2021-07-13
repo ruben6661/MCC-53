01408a01e30585fe8f9a0a069de7df86cfb26c04
JosuaPardosi <josua1811@gmail.com>
2021-07-13 10:42:41 +0700

    Prima.cs
    Program bilangan Prima

---
+++ Prima.cs
@@ -0,0 +1,36 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		 Console.WriteLine("Program Sederhana Bilangan Prima dengan C# .Net \n \n");
+		 Console.Write("Masukkan Batas Bilangan Prima : ");
+	 
+		 bool prima = true;
+		 
+		 int bilangan = int.Parse(Console.ReadLine());
+		 
+		 if (bilangan >= 2)
+		 {
+		 	for (int i = 2; i <= bilangan; i++)
+		 	{
+		 		for (int j = 2; j < i; j++)
+		 		{
+		 			if ((i % j) == 0)
+		 			{
+						 prima = false;
+						 break;
+					 }
+		 		}
+		 
+			 if (prima)
+			 Console.WriteLine("Bilangan " + i + " adalah bilangan prima");
+			 prima = true;
+		 	}
+		 }
+		 else
+		 Console.WriteLine("Tidak ada bilangan prima yang bisa dituliskan");
+		 Console.ReadLine();
+		 }
+}
